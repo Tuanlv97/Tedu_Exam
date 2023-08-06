021b35abc0cb85dd200b7650fdb59406861d07a4
Thợ Code <anhtuan20011997@gmail.com>
2023-08-07 00:55:31 +0700

    Cau hinh lai Identity

---
+++ src/Identitty/Identity.Shared/Dtos/IdentityUserChangePasswordDto.cs
@@ -0,0 +1,10 @@
+﻿
+using Identity.Admin.BusinessLogic.Identity.Dtos.Identity;
+
+namespace Identity.Shared.Dtos
+{
+    public class IdentityUserChangePasswordDto : UserChangePasswordDto<string>
+    {
+        
+    }
+}
